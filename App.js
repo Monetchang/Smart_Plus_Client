1167b26715d7882fc0faeac5a78d18949ccddea3
Bijiabo <bijiabo@gmail.com>
2018-11-01 23:50:50 +0800

    Update: 完成自动下载 Bundle 并跳转页面的逻辑 Add: 添加基础进度条

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ import {
   Button,
 } from "react-native"
 import RNFetchBlob from "rn-fetch-blob"
+import { createStackNavigator } from 'react-navigation'
 import {NativeModules, NativeEventEmitter} from "react-native"
 
 const application = NativeModules.Application
@@ -30,7 +31,25 @@ const instructions = Platform.select({
 })
 
 type Props = {};
-export default class App extends Component<Props> {
+class App extends Component<Props> {
+
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: "",
+      headerStyle: {
+        backgroundColor: '#ffffff',
+        borderBottomWidth: 0,
+        elevation: 0,
+        shadowOpacity: 0,
+      },
+      headerLeft: (
+        <Button title={"Back"} onPress={()=>application.back()}/>
+      ),
+      headerRight: (
+        <Button title={"DEV"} onPress={()=>DevMenu.show()}/>
+      ),
+    };
+  };
 
   constructor(props) {
     super(props)
@@ -41,7 +60,7 @@ export default class App extends Component<Props> {
   }
 
   componentDidMount() {
-
+    this.downloadBundle()
   }
 
   displayDebugMenu = () => {
@@ -72,49 +91,82 @@ export default class App extends Component<Props> {
       // response data will be saved to this path if it has access right.
       path : dirs.DocumentDir + '/singleView.jsbundle'
     })
-      .fetch('GET', 'http://192.168.1.208:8000/singleView.jsbundle')
+      .fetch('GET', 'http://localhost:8000/singleView.jsbundle')
 
     this.downloadTask.progress({ count : 5 }, (received, total) => {
         console.log('progress', received / total)
         this.setState({
-          progress: Math.round(received / total),
+          progress: Math.round(received / total * 100),
         })
       })
       .then((res) => {
+        this.setState({
+          progress: 100,
+        })
         // the temp file path
         console.log('The file saved to ', res)
-        application.restartRCTView()
+        this.loadBundle()
       })
       .catch((err) => {
+        this.setState({
+          progress: 100,
+        })
         // error handling ..
         console.log("download bundle error", err)
+        alert("Bundle download failed! " + err)
       })
+
+    // 30 秒下载超时
+    if (this.downloadBundleTimeout) {
+      clearTimeout(this.downloadBundleTimeout)
+      this.downloadBundleTimeout = undefined
+    }
+    this.downloadBundleTimeout = setTimeout(()=>{
+      this.downloadTask.cancel((err) => {
+        alert("下载超时，请检查网络后重试")
+      })
+      this.downloadBundleTimeout = undefined
+    }, 1000*30)
     // this.downloadTask.cancel((err) => { ... })
+  }
+
+  loadBundle = () => {
+    setTimeout(application.restartRCTView, 1000)
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Progress: {this.state.progress}</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <Button title="Start Download" onPress={this.downloadBundle}/>
 
-        <Button title={"Display Developer Menu"} onPress={this.displayDebugMenu}/>
-        <Button title={"Load New Page"} onPress={this.loadNewPage}/>
-        <Button title={"Back"} onPress={this.back}/>
-        <Button title={"Back Home"} onPress={this.backHome}/>
+        <View style={styles.progressBarContainer}>
+          <View style={{...styles.progressBar, width: `${this.state.progress}%`}} />
+        </View>
+
+        <Text style={styles.instructions}>loading... {this.state.progress}%</Text>
+
+        {/*<Button title="Start Download" onPress={this.downloadBundle}/>*/}
+
+        {/*<Button title={"Display Developer Menu"} onPress={this.displayDebugMenu}/>*/}
+        {/*<Button title={"Load New Page"} onPress={this.loadNewPage}/>*/}
+        {/*<Button title={"Back"} onPress={this.back}/>*/}
+        {/*<Button title={"Back Home"} onPress={this.backHome}/>*/}
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({
+export default createStackNavigator({
+  Home: {
+    screen: App,
+  },
+})
+
+const styles = {
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#F5FCFF",
+    backgroundColor: "#FFFFFF",
   },
   welcome: {
     fontSize: 20,
@@ -125,5 +177,21 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "#333333",
     marginBottom: 5,
+    marginTop: 10,
   },
-})
+  progressBarContainer: {
+    flexDirection: "row",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    height: 8,
+    width: 200,
+    backgroundColor: "#eaeaea",
+    borderRadius: 4,
+  },
+  progressBar: {
+    flexGrow: 0,
+    borderRadius: 4,
+    backgroundColor: "#4FBA80",
+    width: "50%",
+  },
+}
